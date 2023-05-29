e4c5708d6b65f885386f1f61eb223c97666be2a4
Oleksandr <joker.sany13579@gmail.com>
2023-05-29 14:22:51 +0300

    switch case and hours===seconds

---
+++ indexx.js
@@ -0,0 +1,16 @@
+// const nam = prompt('What is your name?')
+// console.log(nam)
+
+// const test = alert('Hello, John! How are you?')
+// console.log(test)
+
+const again =  prompt('What is your name?')
+console.log(again)
+if (again) {
+    prompt ('Hello, John! How are you?')
+    console.log(again)
+}
+else {
+    prompt ('Hello, noname! How are you?')
+    console.log(again)
+}
