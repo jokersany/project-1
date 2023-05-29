4c6e045928e6d8eac33d242cf5301ba155af24cb
Oleksandr <joker.sany13579@gmail.com>
2023-05-29 14:26:34 +0300

    switch case and hours===seconds

--- indexx.js
+++ indexx.js
@@ -1,16 +1,17 @@
-// const nam = prompt('What is your name?')
-// console.log(nam)
-
-// const test = alert('Hello, John! How are you?')
-// console.log(test)
-
-const again =  prompt('What is your name?')
-console.log(again)
-if (again) {
-    prompt ('Hello, John! How are you?')
-    console.log(again)
-}
-else {
-    prompt ('Hello, noname! How are you?')
-    console.log(again)
+const input = +prompt('Enter a number from 1 to 4');
+switch (input) {
+    case 1:
+        alert('Today is a good day to start something');
+        break
+    case 2:
+        alert('See your friend today');  
+        break
+    case 3:
+        alert('Buy yourself something tasty today'); 
+        break
+    case 4:
+        alert('Do your HW today');
+        break
+    default:
+        alert('Invalid number');
 }
